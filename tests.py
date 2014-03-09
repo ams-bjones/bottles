9a4701055259f4432b7eeea5cbc1f23743f0bc60
Brian Jones <bjones@ashmanorschool.com>
2014-03-09 12:28:13 -0400

    started tests

--- tests.py
+++ tests.py
@@ -1,1 +1,12 @@
 #In this file we shall test bottles.py
+import unittest
+import bottles
+
+class tests (unittest.TestCase):
+    def test1(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
